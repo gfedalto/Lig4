b49afedd4f336178eb54b2f2591a33298a6cde79
Geovanni Fedalto <gfedalto@gmail.com>
2023-11-14 14:16:59 -0300

    criação de método para inversão de jogadores

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -15,18 +15,15 @@ public class App
 
         System.out.println("Bem vindo ao liga 4\n");
         System.out.println("Digite o nome do primeiro jogador");
-        String nome1 = sc.nextLine();
+        Jogador jogador1 = new Jogador(sc.nextLine());
         System.out.println("Digite o nome do segundo jogador");
-        String nome2 = sc.nextLine();
-
-        Jogador jogador1 = new Jogador(nome1);
-        Jogador jogador2 = new Jogador(nome2);
+        Jogador jogador2 = new Jogador(sc.nextLine());
 
         var tabuleiro = new Tabuleiro();
-
-        Jogador jogadorDaVez = jogador1;
-        boolean jogadorVenceu;
         Jogada jogada = null;
+        boolean jogadorVenceu;
+        Jogador jogadorDaVez = jogador1;
+
         do {
             //inserir código para limpar o console
             tabuleiro.Exibir();
@@ -52,35 +49,28 @@ public class App
             } catch (ColunaCompletaException ex) {
                 System.out.println(ex.getMessage());
                 Thread.sleep(2000);
-                // tentar eliminar esse monte de inversão com o break <label>
-                // tentar usar uma fila para armazenar e trocar os dois jogadores ao invés do if
-                if (jogadorDaVez == jogador1) {
-                    jogadorDaVez = jogador2;
-                } else {
-                    jogadorDaVez = jogador1;
-                }
+                jogadorDaVez = alternaJogadorDaVez(jogadorDaVez, jogador1, jogador2);
             }
 
             jogadorVenceu = tabuleiro.VerificaVitoria();
-            if (jogadorDaVez == jogador1) {
-                jogadorDaVez = jogador2;
-            } else {
-                jogadorDaVez = jogador1;
-            }
-
+            jogadorDaVez = alternaJogadorDaVez(jogadorDaVez, jogador1, jogador2);
 
             Thread.sleep(1000);
 
-
         } while (!jogadorVenceu);
 
+        jogadorDaVez = alternaJogadorDaVez(jogadorDaVez, jogador1, jogador2);
+        tabuleiro.Exibir();
+        System.out.printf("\nJOGO ENCERRADO!!!!!!!!\nO " +
+                "jogador vencedor foi: %s", jogadorDaVez.getNome());
+    }
+
+    private static Jogador alternaJogadorDaVez(Jogador jogadorDaVez, Jogador jogador1, Jogador jogador2) {
         if (jogadorDaVez == jogador1) {
             jogadorDaVez = jogador2;
         } else {
             jogadorDaVez = jogador1;
         }
-        tabuleiro.Exibir();
-        System.out.printf("\nJOGO ENCERRADO!!!!!!!!\nO " +
-                "jogador vencedor foi: %s", jogadorDaVez.getNome());
+        return jogadorDaVez;
     }
 }
